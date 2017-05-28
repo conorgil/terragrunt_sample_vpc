1702d9a67711ff8721960218b955359a0ccf58e9
Conor Gilsenan <conorgilsenan@gmail.com>
2017-05-28 16:14:36 -0600

    Add additional output variables

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "voc_cidr_block" {
-  value = "aws_vpc.main.cidr_block"
+  value = "${aws_vpc.main.cidr_block}"
+}
+
+output "public_subnet_ids" {
+  value = "${aws_subnet.public.*.id}"
+}
+
+output "private_subnet_ids" {
+  value = "${aws_subnet.private.*.id}"
 }
